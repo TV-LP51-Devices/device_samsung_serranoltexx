7f9d18f6db47c9dee7664236169df15bb6cd3fba
martin coulon <martinusbe@gmail.com>
2015-04-30 06:08:37 +0200

    remove unneeded aapt configs

--- validus.mk
+++ validus.mk
@@ -9,7 +9,3 @@ $(call inherit-product, device/samsung/serranoltexx/full_serranoltexx.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serranoltexx TARGET_DEVICE=serranolte
 
 PRODUCT_NAME := validus_serranoltexx
-
-# Use high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal hdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
